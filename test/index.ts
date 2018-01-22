47cd7b7c328c5ccd63350b3a032c412c3cc938c8
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-22 20:04:06 +0100

    test: integrate Wallet methods checks

--- test/index.ts
+++ test/index.ts
@@ -14,9 +14,29 @@ function testMethodType(name, method) {
 
 describe('Typescript', function() {
   describe('Basics', function() {
-    const electraJs = new ElectraJs()
+    // There is no need for real settings here since this test is only meant to check the methods existence.
+    const electraJs = new ElectraJs({
+      rpcAuth: { username: '', password: '' },
+      rpcUri: ''
+    })
+
+    testMethodType('getVersion', electraJs.getVersion)
 
     testMethodType('api#getCurrentPriceIn', electraJs.api.getCurrentPriceIn)
-    testMethodType('getVersion', electraJs.getVersion)
+
+    testMethodType('wallet#check', electraJs.wallet.check)
+    testMethodType('wallet#getAccount', electraJs.wallet.getAccount)
+    testMethodType('wallet#getBalance', electraJs.wallet.getBalance)
+    testMethodType('wallet#getDifficulty', electraJs.wallet.getDifficulty)
+    testMethodType('wallet#getNewAddress', electraJs.wallet.getNewAddress)
+    testMethodType('wallet#listAddressGroupings', electraJs.wallet.listAddressGroupings)
+    testMethodType('wallet#listReceivedByAddress', electraJs.wallet.listReceivedByAddress)
+    testMethodType('wallet#makeKeyPair', electraJs.wallet.makeKeyPair)
+    testMethodType('wallet#listTransactions', electraJs.wallet.listTransactions)
+    testMethodType('wallet#listUnspent', electraJs.wallet.listUnspent)
+    testMethodType('wallet#lock', electraJs.wallet.lock)
+    testMethodType('wallet#storePassphrase', electraJs.wallet.storePassphrase)
+    testMethodType('wallet#validateAddress', electraJs.wallet.validateAddress)
+    testMethodType('wallet#validatePublicKey', electraJs.wallet.validatePublicKey)
   })
 })
