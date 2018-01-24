805fe2d107fe9b62624e0e00e5b852a57e410101
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-24 21:55:47 +0100

    web-services/getCurrentPriceIn.spec: fix NaN case

--- src/web-services/getCurrentPriceIn.spec.ts
+++ src/web-services/getCurrentPriceIn.spec.ts
@@ -10,14 +10,14 @@ describe('webServices#getCurrentPriceIn()', async function() {
   it(`SHOULD return a number with no parameter (= USD)`, function(done) {
     getCurrentPriceIn().then(price => {
       usdPrice = price
-      assert.strictEqual(typeof usdPrice, 'number')
+      assert.strictEqual(typeof usdPrice === 'number' && !isNaN(usdPrice), true)
       done()
     })
   })
 
   it(`SHOULD return a different number with "JPY" parameter`, function(done) {
     getCurrentPriceIn('JPY').then(eurPrice => {
-      assert.strictEqual(typeof eurPrice, 'number')
+      assert.strictEqual(typeof eurPrice === 'number' && !isNaN(eurPrice), true)
       assert.notStrictEqual(eurPrice, usdPrice)
       done()
     })
