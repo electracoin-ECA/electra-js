efdace59fa497c0a023b030ac65080b033b46f4f
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-24 02:15:55 +0100

    utils/getCurrentPriceIn.spec: lint

--- src/utils/getCurrentPriceIn.spec.ts
+++ src/utils/getCurrentPriceIn.spec.ts
@@ -9,23 +9,19 @@ getCurrentPriceIn()
 describe('utils/getCurrentPriceIn()', async function() {
   let usdPrice
 
-  describe('WITH no parameter (= USD)', function() {
-    it(`SHOULD return a number`, function(done) {
-      getCurrentPriceIn().then(price => {
-        usdPrice = price
-        assert.strictEqual(typeof usdPrice, 'number')
-        done()
-      })
+  it(`SHOULD return a number with no parameter (= USD)`, function(done) {
+    getCurrentPriceIn().then(price => {
+      usdPrice = price
+      assert.strictEqual(typeof usdPrice, 'number')
+      done()
     })
   })
 
-  describe('WITH "JPY" parameter', function() {
-    it(`SHOULD return a different number`, function(done) {
-      getCurrentPriceIn('JPY').then(eurPrice => {
-        assert.strictEqual(typeof eurPrice, 'number')
-        assert.notStrictEqual(eurPrice, usdPrice)
-        done()
-      })
+  it(`SHOULD return a different number with "JPY" parameter`, function(done) {
+    getCurrentPriceIn('JPY').then(eurPrice => {
+      assert.strictEqual(typeof eurPrice, 'number')
+      assert.notStrictEqual(eurPrice, usdPrice)
+      done()
     })
   })
 })
