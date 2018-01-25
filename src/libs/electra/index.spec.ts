9bb9dcc0851e38351faa1ccf30737888f5c0a880
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-25 18:27:58 +0100

    libs/electra: add first HD wallet methods

--- src/libs/electra/index.spec.ts
+++ src/libs/electra/index.spec.ts
@@ -2,32 +2,107 @@
 
 import * as assert from 'assert'
 
-import electra from '.'
+import Electra from '.'
+import * as bip39 from 'bip39'
 
-describe.only('libs/electra', function() {
-  describe(`#createAddress()`, function() {
-    const address = electra.createAddress()
+const MNEMONIC_TEST = 'bridge cigar wheel tent balcony identify predict rose deer avocado clip bracket'
 
-    it(`SHOULD create an object containing a non-empty string address & a non-empty string private key`, function() {
-      assert.strictEqual(typeof address.hash, 'string')
-      assert.strictEqual(address.hash.length > 0, true)
-      assert.strictEqual(typeof address.privateKey, 'string')
-      assert.strictEqual(address.privateKey.length > 0, true)
-    })
-
-    it(`AND its private key should be resolved into the same address via #getAddressfromWif()`, function() {
-      assert.strictEqual(electra.getAddressFromWif(address.privateKey), address.hash)
-    })
-  })
-})
-
-describe('libs/electra', function() {
-  describe(`#getAddressfromWif()`, function() {
+describe('libs/Electra', function() {
+  describe(`#getAddressHashFromPrivateKey()`, function() {
     it(`SHOULD return the expected address`, function() {
       assert.strictEqual(
-        electra.getAddressFromWif('QqDPDtyVoA1wsdB2HkkofMoB567GGxLUEM8RHffvYfQ2UZ2UU6PB'),
+        Electra.getAddressHashFromPrivateKey('QqDPDtyVoA1wsdB2HkkofMoB567GGxLUEM8RHffvYfQ2UZ2UU6PB'),
         'EdiEBrGqDUn6PDeNFKb1aLmei5uHVfmmd2'
       )
     })
   })
+
+  const MNEMONIC_TEST = 'bridge cigar wheel tent balcony identify predict rose deer avocado clip bracket'
+  let accountPrivateKey, mainAddress, masterNodeAddress
+
+  describe(`#getMasterNodeAddressFromMnemonic()`, function() {
+    masterNodeAddress = Electra.getMasterNodeAddressFromMnemonic(MNEMONIC_TEST)
+
+    it(`SHOULD return the expected private key`, function() {
+      assert.strictEqual(masterNodeAddress.privateKey, 'QqwHWeqEQx1yxTM4NHmhB16GxZ8HKDQhV5h9KMqHgZjBJD1wxNBq')
+    })
+    it(`AND SHOULD return the expected hash`, function() {
+      assert.strictEqual(masterNodeAddress.hash, 'EHtiQTEnQnbF4w6qwFV2vZ5rCKdcvSTbK4')
+    })
+    it(`WHICH SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+      assert.strictEqual(
+        Electra.getAddressHashFromPrivateKey(masterNodeAddress.privateKey),
+        masterNodeAddress.hash
+      )
+    })
+  })
+
+  describe(`#getDerivatedAddressFromMasterNodePrivateKey()`, function() {
+    describe(`WHEN looking for the first derivated address (index = 0)`, function() {
+      const firstAddress = Electra.getDerivatedAddressFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0)
+
+      it(`SHOULD return the expected private key`, function() {
+        assert.strictEqual(firstAddress.privateKey, 'QsNh8Yb8UkD3iLSAd5gpoLcomTFzR9Quzordt6UbEM5a9h2rAn6p')
+      })
+      it(`AND SHOULD return the expected hash`, function() {
+        assert.strictEqual(firstAddress.hash, 'EegNGXsWa3HFStTMEjgRCan65KVP41BfRN')
+      })
+      it(`WHICH SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+        assert.strictEqual(
+          Electra.getAddressHashFromPrivateKey(firstAddress.privateKey),
+          firstAddress.hash
+        )
+      })
+    })
+
+    describe(`WHEN looking for the second derivated address (index = 1)`, function() {
+      const secondAddress = Electra.getDerivatedAddressFromMasterNodePrivateKey(masterNodeAddress.privateKey, 1)
+
+      it(`SHOULD return the expected private key`, function() {
+        assert.strictEqual(secondAddress.privateKey, 'QrmU7yAhrNbTRnSz68Dq3qgHCVPZ6YzkzvnJqC1eD4Np35oCjFVS')
+      })
+      it(`AND SHOULD return the expected hash`, function() {
+        assert.strictEqual(secondAddress.hash, 'EXn2p6Vce65F9G5jcQnKhtr8XQyNgmbJk4')
+      })
+      it(`WHICH SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+        assert.strictEqual(
+          Electra.getAddressHashFromPrivateKey(secondAddress.privateKey),
+          secondAddress.hash
+        )
+      })
+    })
+
+    describe(`WHEN looking for the hundredth derivated address (index = 99)`, function() {
+      const hundredthAddress = Electra.getDerivatedAddressFromMasterNodePrivateKey(masterNodeAddress.privateKey, 99)
+
+      it(`SHOULD return the expected private key`, function() {
+        assert.strictEqual(hundredthAddress.privateKey, 'QsucZ6UERsypYk2Wq1CjKwrhza3H6B3KJ5vygzqUb1uUPML1MVGX')
+      })
+      it(`AND SHOULD return the expected hash`, function() {
+        assert.strictEqual(hundredthAddress.hash, 'EUQtCEy1vyHpNgQSiRhjm1c9ddBkqgcwH5')
+      })
+      it(`WHICH SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+        assert.strictEqual(
+          Electra.getAddressHashFromPrivateKey(hundredthAddress.privateKey),
+          hundredthAddress.hash
+        )
+      })
+    })
+  })
+
+  describe(`#getRandomMnemonic()`, function() {
+    const mnemonic = Electra.getRandomMnemonic()
+
+    it(`SHOULD return a non-empty string made of 12 lowercase regular letters`, function() {
+      assert.strictEqual(typeof mnemonic, 'string')
+      assert.strictEqual(mnemonic.length > 0, true)
+      assert.strictEqual(mnemonic.split(' ').length, 12)
+      assert.strictEqual(mnemonic, mnemonic.toLocaleLowerCase())
+    })
+
+    it(`WHICH SHOULD be a valid BIP39 mnemonic`, function() {
+      assert.strictEqual(bip39.validateMnemonic(mnemonic), true)
+    })
+  })
 })
+
--- src/libs/electra/index.ts
+++ src/libs/electra/index.ts
@@ -1,6 +1,12 @@
+// tslint:disable:no-unnecessary-class
+
+import * as bip39 from 'bip39'
 import * as bitcoinJs from 'bitcoinjs-lib'
 
 import { Address } from './types'
+
+const CHAIN_CODE_BUFFER_SIZE: number = 32
+const ENTROPY_STRENGTH: number = 128
 
 const ECA_NETWORK: bitcoinJs.Network = {
   bip32: { public: 0, private: 0 },
@@ -10,20 +16,100 @@ const ECA_NETWORK: bitcoinJs.Network = {
   wif: 161
 }
 
-export default {
-  createAddress: (): Address => {
-    const keyPair: bitcoinJs.ECPair = bitcoinJs.ECPair.makeRandom({ network: ECA_NETWORK })
-    const privateKey: string = keyPair.toWIF()
-    const hash: string = keyPair.getAddress()
-
-    return {
-      hash,
-      privateKey
-    }
-  },
-
-  getAddressFromWif: (privateKey: string): string =>
-    bitcoinJs.ECPair
+/**
+ * Electra blockchain functions.
+ */
+export default class Electra {
+  /**
+   * Resolve the address hash from its WIF private key.
+   */
+  public static getAddressHashFromPrivateKey(privateKey: string): string {
+    return bitcoinJs.ECPair
       .fromWIF(privateKey, ECA_NETWORK)
       .getAddress()
+  }
+
+  /**
+   * Calculate the derivated key n (0-indexed) from a Highly Deterministic Wallet Master Node private key.
+   *
+   * @see https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#master-key-generation
+   */
+  public static getDerivatedAddressFromMasterNodePrivateKey(privateKey: string, derivationIndex: number): Address {
+    const masterNode: bitcoinJs.HDNode = this.getMasterNodeFromPrivateKey(privateKey)
+    const derivatedNode: bitcoinJs.HDNode = masterNode.derive(derivationIndex)
+
+    return {
+      hash: derivatedNode.getAddress(),
+      privateKey: derivatedNode.keyPair.toWIF()
+    }
+  }
+
+  /**
+   * Resolve the Highly Deterministic Wallet Master Node address hash and WIF private key
+   * from its associated mnemonic, extended by the mnemonic extension if given.
+   *
+   * @see https://en.bitcoin.it/wiki/Mnemonic_phrase
+   */
+  public static getMasterNodeAddressFromMnemonic(mnemonic: string, mnemonicExtension?: string): Address {
+    const masterNode: bitcoinJs.HDNode = this.getMasterNodeFromMnemonic(mnemonic, mnemonicExtension)
+    const keyPair: bitcoinJs.ECPair = masterNode.keyPair
+
+    return {
+      hash: keyPair.getAddress(),
+      privateKey: keyPair.toWIF()
+    }
+  }
+
+  /**
+   * Generate a crypto-random address.
+   *
+   * @note This address can't be associated with a mnemonic and requires its private key to be recovered.
+   */
+  public static getRandomAddress(): Address {
+    const keyPair: bitcoinJs.ECPair = bitcoinJs.ECPair.makeRandom({ network: ECA_NETWORK })
+
+    return {
+      hash: keyPair.getAddress(),
+      privateKey: keyPair.toWIF()
+    }
+  }
+
+  /**
+   * Generate a crypto-random mnemonic, using a 128-bits entropy.
+   *
+   * @note A 128-bits entropy generates a 12 words mnemonic.
+   * @see https://github.com/bitcoinjs/bip39
+   */
+  public static getRandomMnemonic(): string {
+    return bip39.generateMnemonic(ENTROPY_STRENGTH)
+  }
+
+  /**
+   * Return an instance of Highly Deterministic Wallet Master Node from its mnemonic,
+   * extended by the mnemonic extension if given.
+   */
+  private static getMasterNodeFromMnemonic(mnemonic: string, mnemonicExtension?: string): bitcoinJs.HDNode {
+    const seed: Buffer = this.getSeedFromMnemonic(mnemonic, mnemonicExtension)
+
+    return bitcoinJs.HDNode.fromSeedBuffer(seed, ECA_NETWORK)
+  }
+
+  /**
+   * Return an instance of Highly Deterministic Wallet Master Node from its WIF private key.
+   */
+  private static getMasterNodeFromPrivateKey(privateKey: string): bitcoinJs.HDNode {
+    const masterNodeKeyPair: bitcoinJs.ECPair = bitcoinJs.ECPair.fromWIF(privateKey, ECA_NETWORK)
+
+    // TODO Check the "chainCode" buffer
+    return new bitcoinJs.HDNode(masterNodeKeyPair, new Buffer(CHAIN_CODE_BUFFER_SIZE))
+  }
+
+  /**
+   * Convert the mnemonic into a seed buffer, extended by the mnemonic extension if given.
+   *
+   * @see https://en.bitcoin.it/wiki/Mnemonic_phrase
+   */
+  private static getSeedFromMnemonic(mnemonic: string, mnemonicExtension?: string): Buffer {
+    return bip39.mnemonicToSeed(mnemonic, mnemonicExtension)
+  }
 }
