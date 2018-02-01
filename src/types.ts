5ca0424b4ff7d666181c8609446485c6c5e917e6
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-01 18:33:55 +0100

    types: add a global Address interface

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,9 @@
 export type OrNull<T> = T | null
 export type PartialOrNull<T> = OrNull<Partial<T>>
+
+export interface Address {
+  hash: string
+  isCiphered: boolean
+  isHD: boolean
+  privateKey: string
+}
