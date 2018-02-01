56efcb8584d8760df0735942c2bf392567136ffe
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-01 18:32:17 +0100

    web-services/index: link getBalanceFor()

--- src/web-services/index.ts
+++ src/web-services/index.ts
@@ -1,9 +1,12 @@
+import getBalanceFor from './getBalanceFor'
 import getCurrentPriceIn from './getCurrentPriceIn'
 
 export interface WebServices {
+  getBalanceFor: typeof getBalanceFor
   getCurrentPriceIn: typeof getCurrentPriceIn
 }
 
 export default {
+  getBalanceFor,
   getCurrentPriceIn
 }
