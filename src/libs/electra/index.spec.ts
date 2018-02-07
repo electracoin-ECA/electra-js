a9f80918535323d7e699e41424d16a75cc4231a3
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-07 23:09:45 +0100

    libs/electra/index.spec: add and fix tests

--- src/libs/electra/index.spec.ts
+++ src/libs/electra/index.spec.ts
@@ -1,26 +1,62 @@
 // tslint:disable
 
 import * as assert from 'assert'
+import * as bip39 from 'bip39'
+import * as dotenv from 'dotenv'
 
 import Electra from '.'
-import * as bip39 from 'bip39'
 
-import { WALLET_TEST } from '../../wallet/index.spec'
+import { HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST } from '../../wallet/index.spec'
+
+// Loads ".env" variables into process.env properties
+dotenv.config()
+
+const {
+  CUSTOM_ADDRESS_HASH_TEST,
+  CUSTOM_ADDRESS_PRIVATE_KEY_TEST,
+  HD_CHAIN_1_HASH_TEST,
+  HD_CHAIN_1_PRIVATE_KEY_TEST,
+  HD_CHAIN_2_HASH_TEST,
+  HD_CHAIN_2_PRIVATE_KEY_TEST,
+  HD_MASTER_NODE_HASH_TEST,
+  HD_MASTER_NODE_PRIVATE_KEY_TEST,
+  HD_MNEMONIC_EXTENSION_TEST,
+  HD_MNEMONIC_TEST,
+  HD_PASSPHRASE_TEST,
+} = process.env
+
+if (([
+  CUSTOM_ADDRESS_HASH_TEST,
+  CUSTOM_ADDRESS_PRIVATE_KEY_TEST,
+  HD_CHAIN_1_HASH_TEST,
+  HD_CHAIN_1_PRIVATE_KEY_TEST,
+  HD_CHAIN_2_HASH_TEST,
+  HD_CHAIN_2_PRIVATE_KEY_TEST,
+  HD_MASTER_NODE_HASH_TEST,
+  HD_MASTER_NODE_PRIVATE_KEY_TEST,
+  HD_MNEMONIC_EXTENSION_TEST,
+  HD_MNEMONIC_TEST,
+  HD_PASSPHRASE_TEST,
+] as any).includes(undefined)) {
+  console.error('Error: You forgot to fill value(s) in your ".env" test wallet data. Please check ".env.sample".')
+  process.exit(1)
+}
 
 describe('libs/Electra', function() {
-  const MNEMONIC_TEST = 'bridge cigar wheel tent balcony identify predict rose deer avocado clip bracket'
-  let accountPrivateKey, mainAddress, masterNodeAddress
+  let firstAddress, masterNodeAddress, mnemonic, secondAddress
 
-  describe(`#getMasterNodeAddressFromMnemonic()`, function() {
-    masterNodeAddress = Electra.getMasterNodeAddressFromMnemonic(MNEMONIC_TEST)
+  describe(`#getMasterNodeAddressFromMnemonic() WITHOUT <mnemonicExtension>`, function() {
+    it(`SHOULD NOT throw any error`, function() {
+      assert.doesNotThrow(() => masterNodeAddress = Electra.getMasterNodeAddressFromMnemonic(HD_MNEMONIC_TEST))
+    })
 
     it(`SHOULD return the expected private key`, function() {
-      assert.strictEqual(masterNodeAddress.privateKey, WALLET_TEST.masterNode.privateKey)
+      assert.strictEqual(masterNodeAddress.privateKey, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.masterNode.privateKey)
     })
-    it(`AND SHOULD return the expected hash`, function() {
-      assert.strictEqual(masterNodeAddress.hash, WALLET_TEST.masterNode.hash)
+    it(`SHOULD return the expected hash`, function() {
+      assert.strictEqual(masterNodeAddress.hash, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.masterNode.hash)
     })
-    it(`WHICH SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+    it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
       assert.strictEqual(
         Electra.getAddressHashFromPrivateKey(masterNodeAddress.privateKey),
         masterNodeAddress.hash
@@ -29,16 +65,18 @@ describe('libs/Electra', function() {
   })
 
   describe(`#getDerivedChainFromMasterNodePrivateKey()`, function() {
-    describe(`WHEN looking for the first derived chain (walletIndex = 0, chainIndex = 0)`, function() {
-      const firstAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 0)
+    describe(`WHEN looking for the first derived chain (<walletIndex> = 0, <chainIndex> = 0)`, function() {
+      it(`SHOULD NOT throw any error`, function() {
+        assert.doesNotThrow(() => firstAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 0))
+      })
 
       it(`SHOULD return the expected private key`, function() {
-        assert.strictEqual(firstAddress.privateKey, WALLET_TEST.chains[0].privateKey)
+        assert.strictEqual(firstAddress.privateKey, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.chains[0].privateKey)
       })
-      it(`AND SHOULD return the expected hash`, function() {
-        assert.strictEqual(firstAddress.hash, WALLET_TEST.chains[0].hash)
+      it(`SHOULD return the expected hash`, function() {
+        assert.strictEqual(firstAddress.hash, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.chains[0].hash)
       })
-      it(`WHICH SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+      it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
         assert.strictEqual(
           Electra.getAddressHashFromPrivateKey(firstAddress.privateKey),
           firstAddress.hash
@@ -46,16 +84,77 @@ describe('libs/Electra', function() {
       })
     })
 
-    describe(`WHEN looking for the second derived chain (walletIndex = 0, chainIndex = 1)`, function() {
-      const secondAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 1)
+    describe(`WHEN looking for the second derived chain (<walletIndex> = 0, <chainIndex> = 1)`, function() {
+      it(`SHOULD NOT throw any error`, function() {
+        assert.doesNotThrow(() => secondAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 1))
+      })
 
       it(`SHOULD return the expected private key`, function() {
-        assert.strictEqual(secondAddress.privateKey, WALLET_TEST.chains[1].privateKey)
+        assert.strictEqual(secondAddress.privateKey, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.chains[1].privateKey)
       })
-      it(`AND SHOULD return the expected hash`, function() {
-        assert.strictEqual(secondAddress.hash, WALLET_TEST.chains[1].hash)
+      it(`SHOULD return the expected hash`, function() {
+        assert.strictEqual(secondAddress.hash, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.chains[1].hash)
       })
-      it(`WHICH SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+      it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+        assert.strictEqual(
+          Electra.getAddressHashFromPrivateKey(secondAddress.privateKey),
+          secondAddress.hash
+        )
+      })
+    })
+  })
+
+  describe(`#getMasterNodeAddressFromMnemonic() WITH <mnemonicExtension>`, function() {
+    it(`SHOULD NOT throw any error`, function() {
+      assert.doesNotThrow(() => masterNodeAddress = Electra.getMasterNodeAddressFromMnemonic(HD_MNEMONIC_TEST, HD_MNEMONIC_EXTENSION_TEST))
+    })
+
+    it(`SHOULD return the expected private key`, function() {
+      assert.strictEqual(masterNodeAddress.privateKey, HD_MASTER_NODE_PRIVATE_KEY_TEST)
+    })
+    it(`SHOULD return the expected hash`, function() {
+      assert.strictEqual(masterNodeAddress.hash, HD_MASTER_NODE_HASH_TEST)
+    })
+    it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+      assert.strictEqual(
+        Electra.getAddressHashFromPrivateKey(masterNodeAddress.privateKey),
+        masterNodeAddress.hash
+      )
+    })
+  })
+
+  describe(`#getDerivedChainFromMasterNodePrivateKey()`, function() {
+    describe(`WHEN looking for the first derived chain (<walletIndex = 0>, <chainIndex> = 0)`, function() {
+      it(`SHOULD NOT throw any error`, function() {
+        assert.doesNotThrow(() => firstAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 0))
+      })
+
+      it(`SHOULD return the expected private key`, function() {
+        assert.strictEqual(firstAddress.privateKey, HD_CHAIN_1_PRIVATE_KEY_TEST)
+      })
+      it(`SHOULD return the expected hash`, function() {
+        assert.strictEqual(firstAddress.hash, HD_CHAIN_1_HASH_TEST)
+      })
+      it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
+        assert.strictEqual(
+          Electra.getAddressHashFromPrivateKey(firstAddress.privateKey),
+          firstAddress.hash
+        )
+      })
+    })
+
+    describe(`WHEN looking for the second derived chain (<walletIndex> = 0, <chainIndex> = 1)`, function() {
+      it(`SHOULD NOT throw any error`, function() {
+        assert.doesNotThrow(() => secondAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 1))
+      })
+
+      it(`SHOULD return the expected private key`, function() {
+        assert.strictEqual(secondAddress.privateKey, HD_CHAIN_2_PRIVATE_KEY_TEST)
+      })
+      it(`SHOULD return the expected hash`, function() {
+        assert.strictEqual(secondAddress.hash, HD_CHAIN_2_HASH_TEST)
+      })
+      it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
         assert.strictEqual(
           Electra.getAddressHashFromPrivateKey(secondAddress.privateKey),
           secondAddress.hash
@@ -65,7 +164,9 @@ describe('libs/Electra', function() {
   })
 
   describe(`#getRandomMnemonic()`, function() {
-    const mnemonic = Electra.getRandomMnemonic()
+    it(`SHOULD NOT throw any error`, function() {
+      assert.doesNotThrow(() => mnemonic = Electra.getRandomMnemonic())
+    })
 
     it(`SHOULD return a non-empty string made of 12 lowercase regular letters`, function() {
       assert.strictEqual(typeof mnemonic, 'string')
@@ -74,7 +175,7 @@ describe('libs/Electra', function() {
       assert.strictEqual(mnemonic, mnemonic.toLocaleLowerCase())
     })
 
-    it(`WHICH SHOULD be a valid BIP39 mnemonic`, function() {
+    it(`SHOULD be a valid BIP39 mnemonic`, function() {
       assert.strictEqual(bip39.validateMnemonic(mnemonic), true)
     })
   })
