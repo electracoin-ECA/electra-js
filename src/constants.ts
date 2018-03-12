452e5b211597add55788b2029f7acf171d34f55d
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-12 17:18:42 +0100

    constants: add DAEMON_CONFIG & DAEMON_URI

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,15 @@
 import * as bitcoinJs from 'bitcoinjs-lib'
 
+import { DaemonConfig } from './types'
+
+export const DAEMON_CONFIG: DaemonConfig = {
+  rpcpassword: 'pass',
+  rpcport: '5789',
+  rpcuser: 'user'
+}
+
+export const DAEMON_URI: string = `http://127.0.0.1:${DAEMON_CONFIG.rpcport}`
+
 export const ECA_NETWORK: bitcoinJs.Network = {
   bip32: { public: 0, private: 0 },
   messagePrefix: '\u0018Electra very Signed Message:\n', // TODO Not sure about that yet !
