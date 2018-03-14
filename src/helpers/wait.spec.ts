a69ced334f89b363ba358346ddf34db65d73ffb8
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-14 20:47:12 +0100

    helpers/wait.spec: lint

--- src/helpers/wait.spec.ts
+++ src/helpers/wait.spec.ts
@@ -5,7 +5,7 @@ import * as assert from 'assert'
 import wait from './wait'
 
 describe('helpers/wait()', function() {
-  it(`SHOULD return the result`, async function() {
+  it(`SHOULD wait for 1s`, async function() {
     const time = +Date.now()
     await wait(1000)
     assert.strictEqual(+Date.now() >= time + 1000, true)
