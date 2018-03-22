55adaec3f0f1bc07fd4af9bdda67624d84344fbd
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-22 01:39:06 +0100

    helpers/closeElectraDaemons: fix and improve

--- src/helpers/closeElectraDaemons.ts
+++ src/helpers/closeElectraDaemons.ts
@@ -30,59 +30,36 @@ function toArrayOfLines(output: string): string[] {
 }
 
 export default async function(): Promise<void> {
+  const rpc = new Rpc(DAEMON_URI, {
+    password: DAEMON_CONFIG.rpcpassword,
+    username: DAEMON_CONFIG.rpcuser,
+  })
+
   if (['darwin', 'linux'].includes(process.platform)) {
-    /*
-      STEP 1: Find all running Electra deamons
-    */
-
-    const [err1, stdout1] = await to(exec(`ps aux | grep -i "electra"`))
-    if (err1 !== null || stdout1 === undefined) throw err1
-    const resultsRaw1 = toArrayOfLines(stdout1)
-
-    // Remove all "closeElectraDaemons" & "grep" results (since it's just the inlusion of our command)
-    const results1 = resultsRaw1.filter(resultRaw => !/closeElectraDaemons|grep/i.test(resultRaw))
-
-    if (results1.length === 0) return
-
-    /*
-      STEP 2: Find all used RPC port (default one)
-    */
-
-    const [err2, stdout2] = await to(exec(`lsof | grep :${DAEMON_CONFIG.rpcport}`))
-    if (err2 === null && stdout2 !== undefined) {
-      const resultsRaw2 = toArrayOfLines(stdout2)
-
-      // Remove all "closeElectraDaemons" & "grep" results (since it's just the inlusion of our command)
-      const results2 = resultsRaw2.filter(resultRaw => !/closeElectraDaemons|grep/i.test(resultRaw))
-
+   const [err1, stdout1] = await to(exec(`lsof | grep :${DAEMON_CONFIG.rpcport}`))
+   if (err1 === null && stdout1 !== undefined) {
+     const results1 = toArrayOfLines(stdout1)
       // If we find at least one used RPC port, we can try to send a "stop" command
-      if (results2.length !== 0) {
-        const rpc = new Rpc(DAEMON_URI, {
-          password: DAEMON_CONFIG.rpcpassword,
-          username: DAEMON_CONFIG.rpcuser,
-        })
+      if (results1.length !== 0) {
+        const daemonPid = Number(results1[0].split(/\s+/)[1])
 
-        await rpc.stop()
+        try { await rpc.stop() }
+        catch(err) { /* We can ignore any error here. */ }
+
+        // tslint:disable-next-line:no-require-imports
+        require('tree-kill')(daemonPid, 'SIGKILL')
       }
     }
 
-    /*
-      STEP 3: Find all running Electra deamons, again
-    */
+    const [err2, stdout2] = await to(exec(`lsof | grep :${DAEMON_CONFIG.rpcport}`))
+    if (err2 === null && stdout2 !== undefined) {
+      const results2 = toArrayOfLines(stdout2)
 
-    const [err3, stdout3] = await to(exec(`ps aux | grep -i "electra"`))
-    if (err3 !== null || stdout3 === undefined) throw err3
-    const resultsRaw3 = toArrayOfLines(stdout3)
+      if (results2.length === 0) return
+    } else {
+      return
+    }
 
-    // Remove all "closeElectraDaemons" & "grep" results (since it's just the inlusion of our command)
-    const results3 = resultsRaw3.filter(resultRaw => !/closeElectraDaemons|grep/i.test(resultRaw))
-
-    if (results3.length === 0) return
-
-    /*
-      STEP 4: Kill Electra daemons processes
-    */
-
-    await to(exec(`ps -ef | grep -ei electra -ei electrad | grep -v grep | awk '{print $2}' | xargs -r kill -9`))
+    await to(exec(`ps -ef | grep -i ^electra* | grep -v grep | awk '{print $2}' | xargs -r kill -9`))
   }
 }
