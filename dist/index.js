945518da915570c134e25ecfa4d6ac1b430e0da0
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-22 19:16:30 +0100

    0.5.6

--- dist/index.js
+++ dist/index.js
@@ -11486,7 +11486,7 @@ const SETTINGS_DEFAULT = {
  * ElectraJs version.
  * DO NOT CHANGE THIS LINE SINCE THE VERSION IS AUTOMATICALLY INJECTED !
  */
-const VERSION = '0.5.5';
+const VERSION = '0.5.6';
 /**
  * Main ElectraJS class.
  */
