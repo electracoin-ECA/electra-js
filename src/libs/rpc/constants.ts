11bf9e801ac4fdc696bbfd4ea2778184c678fc0e
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-23 16:27:58 +0100

    libs/rpc: invert RPC_ERRORS_TRANSLATION key/value

--- src/libs/rpc/constants.ts
+++ src/libs/rpc/constants.ts
@@ -3,5 +3,5 @@ import { ElectraJsErrorReference } from '../error/types'
 export const RPC_ERRORS_TRANSLATION: {
   [code: string]: keyof ElectraJsErrorReference
 } = {
-  '-32601': '301', // "Method not found"
+  '-32601': 'DAEMON_RPC_METHOD_NOT_FOUND', // "Method not found"
 }
