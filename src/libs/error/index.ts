83ecaa47092b4428b28c4cab1b2ec2e0dc143302
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-23 16:38:49 +0100

    libs/error: comment

--- src/libs/error/index.ts
+++ src/libs/error/index.ts
@@ -1,7 +1,11 @@
 import { ERRORS } from './constants'
 import { ElectraJsErrorReference } from './types'
 
+/**
+ * Custom ElectraJs error class.
+ */
 export default class ElectraJsError<T extends keyof ElectraJsErrorReference> extends Error {
+  /** Error code. */
   public code: ElectraJsErrorReference[T]
 
   public constructor(key: T) {
