71ae46da7495278bf399c165ea817ac8c4a91df8
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-24 23:55:30 +0100

    helpers/closeElectraDaemons: handle Windows case

--- src/helpers/closeElectraDaemons.ts
+++ src/helpers/closeElectraDaemons.ts
@@ -4,6 +4,8 @@ import to from 'await-to-js'
 
 import { DAEMON_CONFIG, DAEMON_URI } from '../constants'
 import Rpc from '../libs/rpc'
+import isPortAvailable from './isPortAvailable'
+import wait from './wait'
 
 async function exec(command: string): Promise<string> {
   return new Promise((resolve: (stdout: string) => void, reject) => {
@@ -35,31 +37,40 @@ export default async function(): Promise<void> {
     username: DAEMON_CONFIG.rpcuser,
   })
 
-  if (['darwin', 'linux'].includes(process.platform)) {
-   const [err1, stdout1] = await to(exec(`lsof | grep :${DAEMON_CONFIG.rpcport}`))
-   if (err1 === null && stdout1 !== undefined) {
-     const results1 = toArrayOfLines(stdout1)
-      // If we find at least one used RPC port, we can try to send a "stop" command
-      if (results1.length !== 0) {
-        const daemonPid = Number(results1[0].split(/\s+/)[1])
+  // First, we can try to send a simple "stop" command
+  try {
+    await rpc.stop()
 
-        try { await rpc.stop() }
-        catch(err) { /* We can ignore any error here. */ }
-
-        // tslint:disable-next-line:no-require-imports
-        require('tree-kill')(daemonPid, 'SIGKILL')
-      }
-    }
-
-    const [err2, stdout2] = await to(exec(`lsof | grep :${DAEMON_CONFIG.rpcport}`))
-    if (err2 === null && stdout2 !== undefined) {
-      const results2 = toArrayOfLines(stdout2)
-
-      if (results2.length === 0) return
-    } else {
-      return
-    }
-
-    await to(exec(`ps -ef | grep -i ^electra* | grep -v grep | awk '{print $2}' | xargs -r kill -9`))
+    // Let's wait for 2s to let the daemon close
+    await wait(2000)
   }
+  catch(err) { /* We can ignore any error here. */ }
+
+  if (isPortAvailable(Number(DAEMON_CONFIG.rpcport))) return
+
+  if (process.platform === 'win32') {
+    // Last resort force kill
+    await to(exec(`FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :${DAEMON_CONFIG.rpcport}') DO TaskKill.exe /PID %%P`))
+  }
+
+  const [err, stdout] = await to(exec(`lsof | grep :${DAEMON_CONFIG.rpcport}`))
+  if (err === null && stdout !== undefined) {
+    const results1 = toArrayOfLines(stdout)
+    // If we find at least one used RPC port, we can try to send a "stop" command
+    if (results1.length !== 0) {
+      const daemonPid = Number(results1[0].split(/\s+/)[1])
+
+      try { await rpc.stop() }
+      catch(err) { /* We can ignore any error here. */ }
+
+      // tslint:disable-next-line:no-require-imports
+      require('tree-kill')(daemonPid, 'SIGKILL')
+    }
+  }
+
+  if (isPortAvailable(Number(DAEMON_CONFIG.rpcport))) return
+
+  // Last resort force kill
+  // TODO Is it a possible case after a tree-kill ?
+  await to(exec(`ps -ef | grep -i ^electra* | grep -v grep | awk '{print $2}' | xargs -r kill -9`))
 }
