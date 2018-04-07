d496f0751e70735d50bcb74fca0487e718b6f1b3
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-07 14:38:38 +0200

    helpers/assertThen: log errors when they happen

--- src/helpers/assertThen.ts
+++ src/helpers/assertThen.ts
@@ -5,7 +5,10 @@ import * as assert from 'assert'
  */
 export default async function<T>(call: () => Promise<T>): Promise<void> {
   let hasError: boolean = false
-  await call().catch(() => hasError = true)
+  await call().catch((err: Error | string) => {
+    console.error(err)
+    hasError = true
+  })
 
   assert.strictEqual(hasError, false)
 }
