5be60149409b4b6cd83149682eb879173692e404
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-11 17:15:09 +0200

    libs/electra: full integration of BIP 44 HD addresses

--- src/libs/electra/index.spec.ts
+++ src/libs/electra/index.spec.ts
@@ -6,16 +6,35 @@ import * as dotenv from 'dotenv'
 
 import Electra from '.'
 
-import { HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST } from '../../wallet/hard/index.spec'
-
 // Loads ".env" variables into process.env properties
 dotenv.config()
 
 const {
-  HD_CHAIN_1_HASH_TEST,
-  HD_CHAIN_1_PRIVATE_KEY_TEST,
-  HD_CHAIN_2_HASH_TEST,
-  HD_CHAIN_2_PRIVATE_KEY_TEST,
+  HD_PURSE_1_EXTERNAL_HASH_TEST,
+  HD_PURSE_1_EXTERNAL_PK_TEST,
+  HD_PURSE_1_CHANGE_HASH_TEST,
+  HD_PURSE_1_CHANGE_PK_TEST,
+  HD_PURSE_2_EXTERNAL_HASH_TEST,
+  HD_PURSE_2_EXTERNAL_PK_TEST,
+  HD_PURSE_2_CHANGE_HASH_TEST,
+  HD_PURSE_2_CHANGE_PK_TEST,
+  HD_CHECKING_1_EXTERNAL_HASH_TEST,
+  HD_CHECKING_1_EXTERNAL_PK_TEST,
+  HD_CHECKING_1_CHANGE_HASH_TEST,
+  HD_CHECKING_1_CHANGE_PK_TEST,
+  HD_CHECKING_2_EXTERNAL_HASH_TEST,
+  HD_CHECKING_2_EXTERNAL_PK_TEST,
+  HD_CHECKING_2_CHANGE_HASH_TEST,
+  HD_CHECKING_2_CHANGE_PK_TEST,
+  HD_SAVINGS_1_EXTERNAL_HASH_TEST,
+  HD_SAVINGS_1_EXTERNAL_PK_TEST,
+  HD_SAVINGS_1_CHANGE_HASH_TEST,
+  HD_SAVINGS_1_CHANGE_PK_TEST,
+  HD_SAVINGS_2_EXTERNAL_HASH_TEST,
+  HD_SAVINGS_2_EXTERNAL_PK_TEST,
+  HD_SAVINGS_2_CHANGE_HASH_TEST,
+  HD_SAVINGS_2_CHANGE_PK_TEST,
+
   HD_MASTER_NODE_HASH_TEST,
   HD_MASTER_NODE_PRIVATE_KEY_TEST,
   HD_MNEMONIC_EXTENSION_TEST,
@@ -25,86 +44,34 @@ const {
   RANDOM_ADDRESS_PRIVATE_KEY_TEST,
 } = process.env
 
-if (([
-  HD_CHAIN_1_HASH_TEST,
-  HD_CHAIN_1_PRIVATE_KEY_TEST,
-  HD_CHAIN_2_HASH_TEST,
-  HD_CHAIN_2_PRIVATE_KEY_TEST,
-  HD_MASTER_NODE_HASH_TEST,
-  HD_MASTER_NODE_PRIVATE_KEY_TEST,
-  HD_MNEMONIC_EXTENSION_TEST,
-  HD_MNEMONIC_TEST,
-  HD_PASSPHRASE_TEST,
-  RANDOM_ADDRESS_HASH_TEST,
-  RANDOM_ADDRESS_PRIVATE_KEY_TEST,
-] as any).includes(undefined)) {
-  console.error('Error: You forgot to fill value(s) in your ".env" test wallet data. Please check ".env.sample".')
-  process.exit(1)
-}
-
 describe('libs/Electra', function() {
-  let firstAddress, masterNodeAddress, mnemonic, secondAddress
+  let purseExternalAddress1, purseChangeAddress1,
+      purseExternalAddress2, purseChangeAddress2,
+      checkingExternalAddress1, checkingChangeAddress1,
+      checkingExternalAddress2, checkingChangeAddress2,
+      savingsExternalAddress1, savingsChangeAddress1,
+      savingsExternalAddress2, savingsChangeAddress2,
+      masterNodeAddress, mnemonic
+
+  describe(`#getAddressHashFromPrivateKey()`, function() {
+    it(`SHOULD resolve back these 13 addresses`, function() {
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_PURSE_1_EXTERNAL_PK_TEST), HD_PURSE_1_EXTERNAL_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_PURSE_1_CHANGE_PK_TEST), HD_PURSE_1_CHANGE_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_PURSE_2_EXTERNAL_PK_TEST), HD_PURSE_2_EXTERNAL_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_PURSE_2_CHANGE_PK_TEST), HD_PURSE_2_CHANGE_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_CHECKING_1_EXTERNAL_PK_TEST), HD_CHECKING_1_EXTERNAL_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_CHECKING_1_CHANGE_PK_TEST), HD_CHECKING_1_CHANGE_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_CHECKING_2_EXTERNAL_PK_TEST), HD_CHECKING_2_EXTERNAL_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_CHECKING_2_CHANGE_PK_TEST), HD_CHECKING_2_CHANGE_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_SAVINGS_1_EXTERNAL_PK_TEST), HD_SAVINGS_1_EXTERNAL_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_SAVINGS_1_CHANGE_PK_TEST), HD_SAVINGS_1_CHANGE_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_SAVINGS_2_EXTERNAL_PK_TEST), HD_SAVINGS_2_EXTERNAL_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(HD_SAVINGS_2_CHANGE_PK_TEST), HD_SAVINGS_2_CHANGE_HASH_TEST)
+      assert.strictEqual(Electra.getAddressHashFromPrivateKey(RANDOM_ADDRESS_PRIVATE_KEY_TEST), RANDOM_ADDRESS_HASH_TEST)
+    })
+  })
 
   describe(`#getMasterNodeAddressFromMnemonic() WITHOUT <mnemonicExtension>`, function() {
-    it(`SHOULD NOT throw any error`, function() {
-      assert.doesNotThrow(() => masterNodeAddress = Electra.getMasterNodeAddressFromMnemonic(HD_MNEMONIC_TEST))
-    })
-
-    it(`SHOULD return the expected private key`, function() {
-      assert.strictEqual(masterNodeAddress.privateKey, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.masterNode.privateKey)
-    })
-    it(`SHOULD return the expected hash`, function() {
-      assert.strictEqual(masterNodeAddress.hash, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.masterNode.hash)
-    })
-    it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
-      assert.strictEqual(
-        Electra.getAddressHashFromPrivateKey(masterNodeAddress.privateKey),
-        masterNodeAddress.hash
-      )
-    })
-  })
-
-  describe(`#getDerivedChainFromMasterNodePrivateKey()`, function() {
-    describe(`WHEN looking for the first derived chain (<walletIndex> = 0, <chainIndex> = 0)`, function() {
-      it(`SHOULD NOT throw any error`, function() {
-        assert.doesNotThrow(() => firstAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 0))
-      })
-
-      it(`SHOULD return the expected private key`, function() {
-        assert.strictEqual(firstAddress.privateKey, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.chains[0].privateKey)
-      })
-      it(`SHOULD return the expected hash`, function() {
-        assert.strictEqual(firstAddress.hash, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.chains[0].hash)
-      })
-      it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
-        assert.strictEqual(
-          Electra.getAddressHashFromPrivateKey(firstAddress.privateKey),
-          firstAddress.hash
-        )
-      })
-    })
-
-    describe(`WHEN looking for the second derived chain (<walletIndex> = 0, <chainIndex> = 1)`, function() {
-      it(`SHOULD NOT throw any error`, function() {
-        assert.doesNotThrow(() => secondAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 1))
-      })
-
-      it(`SHOULD return the expected private key`, function() {
-        assert.strictEqual(secondAddress.privateKey, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.chains[1].privateKey)
-      })
-      it(`SHOULD return the expected hash`, function() {
-        assert.strictEqual(secondAddress.hash, HD_WALLET_WITHOUT_MNEMONIC_EXTENSION_TEST.chains[1].hash)
-      })
-      it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
-        assert.strictEqual(
-          Electra.getAddressHashFromPrivateKey(secondAddress.privateKey),
-          secondAddress.hash
-        )
-      })
-    })
-  })
-
-  describe(`#getMasterNodeAddressFromMnemonic() WITH <mnemonicExtension>`, function() {
     it(`SHOULD NOT throw any error`, function() {
       assert.doesNotThrow(() => masterNodeAddress = Electra.getMasterNodeAddressFromMnemonic(HD_MNEMONIC_TEST, HD_MNEMONIC_EXTENSION_TEST))
     })
@@ -115,50 +82,53 @@ describe('libs/Electra', function() {
     it(`SHOULD return the expected hash`, function() {
       assert.strictEqual(masterNodeAddress.hash, HD_MASTER_NODE_HASH_TEST)
     })
-    it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
-      assert.strictEqual(
-        Electra.getAddressHashFromPrivateKey(masterNodeAddress.privateKey),
-        masterNodeAddress.hash
-      )
-    })
   })
 
   describe(`#getDerivedChainFromMasterNodePrivateKey()`, function() {
-    describe(`WHEN looking for the first derived chain (<walletIndex = 0>, <chainIndex> = 0)`, function() {
+    describe(`WHEN deriving its accounts (0, 1 and 2), chains (0, 1 and 2), and their change`, function() {
       it(`SHOULD NOT throw any error`, function() {
-        assert.doesNotThrow(() => firstAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 0))
+        assert.doesNotThrow(() => purseExternalAddress1 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 0, false))
+        assert.doesNotThrow(() => purseChangeAddress1 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 0, true))
+        assert.doesNotThrow(() => purseExternalAddress2 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 1, false))
+        assert.doesNotThrow(() => purseChangeAddress2 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 1, true))
+        assert.doesNotThrow(() => checkingExternalAddress1 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 1, 0, false))
+        assert.doesNotThrow(() => checkingChangeAddress1 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 1, 0, true))
+        assert.doesNotThrow(() => checkingExternalAddress2 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 1, 1, false))
+        assert.doesNotThrow(() => checkingChangeAddress2 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 1, 1, true))
+        assert.doesNotThrow(() => savingsExternalAddress1 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 2, 0, false))
+        assert.doesNotThrow(() => savingsChangeAddress1 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 2, 0, true))
+        assert.doesNotThrow(() => savingsExternalAddress2 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 2, 1, false))
+        assert.doesNotThrow(() => savingsChangeAddress2 = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 2, 1, true))
       })
 
-      it(`SHOULD return the expected private key`, function() {
-        assert.strictEqual(firstAddress.privateKey, HD_CHAIN_1_PRIVATE_KEY_TEST)
-      })
-      it(`SHOULD return the expected hash`, function() {
-        assert.strictEqual(firstAddress.hash, HD_CHAIN_1_HASH_TEST)
-      })
-      it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
-        assert.strictEqual(
-          Electra.getAddressHashFromPrivateKey(firstAddress.privateKey),
-          firstAddress.hash
-        )
-      })
-    })
-
-    describe(`WHEN looking for the second derived chain (<walletIndex> = 0, <chainIndex> = 1)`, function() {
-      it(`SHOULD NOT throw any error`, function() {
-        assert.doesNotThrow(() => secondAddress = Electra.getDerivedChainFromMasterNodePrivateKey(masterNodeAddress.privateKey, 0, 1))
+      it(`SHOULD return the expected private keys`, function() {
+        assert.strictEqual(purseExternalAddress1.privateKey, HD_PURSE_1_EXTERNAL_PK_TEST)
+        assert.strictEqual(purseChangeAddress1.privateKey, HD_PURSE_1_CHANGE_PK_TEST)
+        assert.strictEqual(purseExternalAddress2.privateKey, HD_PURSE_2_EXTERNAL_PK_TEST)
+        assert.strictEqual(purseChangeAddress2.privateKey, HD_PURSE_2_CHANGE_PK_TEST)
+        assert.strictEqual(checkingExternalAddress1.privateKey, HD_CHECKING_1_EXTERNAL_PK_TEST)
+        assert.strictEqual(checkingChangeAddress1.privateKey, HD_CHECKING_1_CHANGE_PK_TEST)
+        assert.strictEqual(checkingExternalAddress2.privateKey, HD_CHECKING_2_EXTERNAL_PK_TEST)
+        assert.strictEqual(checkingChangeAddress2.privateKey, HD_CHECKING_2_CHANGE_PK_TEST)
+        assert.strictEqual(savingsExternalAddress1.privateKey, HD_SAVINGS_1_EXTERNAL_PK_TEST)
+        assert.strictEqual(savingsChangeAddress1.privateKey, HD_SAVINGS_1_CHANGE_PK_TEST)
+        assert.strictEqual(savingsExternalAddress2.privateKey, HD_SAVINGS_2_EXTERNAL_PK_TEST)
+        assert.strictEqual(savingsChangeAddress2.privateKey, HD_SAVINGS_2_CHANGE_PK_TEST)
       })
 
-      it(`SHOULD return the expected private key`, function() {
-        assert.strictEqual(secondAddress.privateKey, HD_CHAIN_2_PRIVATE_KEY_TEST)
-      })
-      it(`SHOULD return the expected hash`, function() {
-        assert.strictEqual(secondAddress.hash, HD_CHAIN_2_HASH_TEST)
-      })
-      it(`SHOULD be resolved back via #getAddressHashFromPrivateKey()`, function() {
-        assert.strictEqual(
-          Electra.getAddressHashFromPrivateKey(secondAddress.privateKey),
-          secondAddress.hash
-        )
+      it(`SHOULD return the expected hashes`, function() {
+        assert.strictEqual(purseExternalAddress1.hash, HD_PURSE_1_EXTERNAL_HASH_TEST)
+        assert.strictEqual(purseChangeAddress1.hash, HD_PURSE_1_CHANGE_HASH_TEST)
+        assert.strictEqual(purseExternalAddress2.hash, HD_PURSE_2_EXTERNAL_HASH_TEST)
+        assert.strictEqual(purseChangeAddress2.hash, HD_PURSE_2_CHANGE_HASH_TEST)
+        assert.strictEqual(checkingExternalAddress1.hash, HD_CHECKING_1_EXTERNAL_HASH_TEST)
+        assert.strictEqual(checkingChangeAddress1.hash, HD_CHECKING_1_CHANGE_HASH_TEST)
+        assert.strictEqual(checkingExternalAddress2.hash, HD_CHECKING_2_EXTERNAL_HASH_TEST)
+        assert.strictEqual(checkingChangeAddress2.hash, HD_CHECKING_2_CHANGE_HASH_TEST)
+        assert.strictEqual(savingsExternalAddress1.hash, HD_SAVINGS_1_EXTERNAL_HASH_TEST)
+        assert.strictEqual(savingsChangeAddress1.hash, HD_SAVINGS_1_CHANGE_HASH_TEST)
+        assert.strictEqual(savingsExternalAddress2.hash, HD_SAVINGS_2_EXTERNAL_HASH_TEST)
+        assert.strictEqual(savingsChangeAddress2.hash, HD_SAVINGS_2_CHANGE_HASH_TEST)
       })
     })
   })
--- src/libs/electra/index.ts
+++ src/libs/electra/index.ts
@@ -9,6 +9,8 @@ import { Address } from '../../types'
 
 const CHAIN_CODE_BUFFER_SIZE: number = 32
 const ENTROPY_STRENGTH: number = 256
+const HD_DERIVATION_COIN_TYPE: number = 0
+const HD_DERIVATION_PURPOSE: number = 44
 
 /**
  * Electra blockchain functions.
@@ -24,17 +26,24 @@ export default class Electra {
   }
 
   /**
-   * Calculate the derived key n (0-indexed) from a Highly Deterministic Wallet Master Node private key.
+   * Calculate the derived key address (0-indexed) from a Hierarchical Deterministic Wallet Master Node private key.
+   * Recall: m / purpose' / coin_type' / account' / change / address_index
    *
-   * @see https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#master-key-generation
+   * @see https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
+   * @see https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
+   * @see https://github.com/bitcoin/bips/blob/master/bip-0043.mediawiki
+   * @see https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
    */
   public static getDerivedChainFromMasterNodePrivateKey(
     privateKey: string,
-    walletIndex: number,
-    chainIndex: number
+    accountIndex: number,
+    addressIndex: number,
+    isChange: boolean,
   ): Address {
     const masterNode: bitcoinJs.HDNode = this.getMasterNodeFromPrivateKey(privateKey)
-    const derivedNode: bitcoinJs.HDNode = masterNode.deriveHardened(walletIndex).derive(chainIndex)
+    const derivedNode: bitcoinJs.HDNode = masterNode.derivePath(
+      `m/${HD_DERIVATION_PURPOSE}'/${HD_DERIVATION_COIN_TYPE}'/${accountIndex}'/${Number(isChange)}/${addressIndex}`
+    )
 
     return {
       hash: derivedNode.getAddress(),
@@ -48,6 +57,7 @@ export default class Electra {
    * Resolve the Highly Deterministic Wallet Master Node address hash and WIF private key
    * from its associated mnemonic, extended by the mnemonic extension if given.
    *
+   * @see https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
    * @see https://en.bitcoin.it/wiki/Mnemonic_phrase
    */
   public static getMasterNodeAddressFromMnemonic(mnemonic: string, mnemonicExtension?: string): Address {
@@ -82,6 +92,7 @@ export default class Electra {
    * Generate a crypto-random mnemonic, using a 256-bits entropy.
    *
    * @note A 256-bits entropy generates a 24 words mnemonic.
+   * @see https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
    * @see https://github.com/bitcoinjs/bip39
    */
   public static getRandomMnemonic(): string {
