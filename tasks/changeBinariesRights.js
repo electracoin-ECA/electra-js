dacda44ef24b46c7d7f8d8117c1bd6d45c4b10b9
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-04 19:48:41 +0200

    tasks/changeBinariesRights: lint

--- tasks/changeBinariesRights.js
+++ tasks/changeBinariesRights.js
@@ -1,20 +1,14 @@
 const childProcess = require('child_process')
 const path = require('path')
 
-if (process.platform === 'darwin') {
-  console.log('Updating MacOS binary rights...')
-  const process = childProcess
-    .spawn('chmod', [
-      '755',
-      path.resolve(__dirname, '../bin/electrad-macos'),
-    ])
-}
+const binaryPath = path.resolve(
+  __dirname,
+  '..',
+  'bin',
+  `electrad-${process.platform}-${process.arch}${process.platform === 'win32' ? '.exe' : ''}`
+)
 
-if (process.platform === 'linux') {
-  console.log('Updating Linux binary rights...')
-  const process = childProcess
-    .spawn('chmod', [
-      '755',
-      path.resolve(__dirname, '../bin/electrad-linux'),
-    ])
+if (['darwin', 'linux'].includes(process.platform)) {
+  console.log('Updating MacOS binary rights...')
+  const process = childProcess.spawn('chmod', ['755', binaryPath])
 }
