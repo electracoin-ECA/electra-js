4ef1b7b3a9f58dad3310d34682dbdc02ceaabde3
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-05 00:56:00 +0200

    types: fill all DaemonConfig paramaters

--- src/types.ts
+++ src/types.ts
@@ -11,10 +11,73 @@ export interface Address {
 }
 
 export interface DaemonConfig {
+  addnode: string
+  alertnotify: string
+  banscore: string
+  bantime: string
+  bind: string
+  blockmaxsize: string
+  blockminsize: string
+  blocknotify: string
+  blockprioritysize: string
+  checkblocks: string
+  checklevel: string
+  conf: string
+  confchange: boolean
+  connect: string
+  cppolicy: boolean
+  daemon: boolean
+  datadir: string
+  dbcache: string
+  dblogsize: string
+  debug: boolean
+  debugnet: boolean
+  detachdb: boolean
+  discover: boolean
+  dns: boolean
+  dnsseed: boolean
+  enforcecanonical: boolean
+  externalip: string
+  irc: boolean
+  keypool: string
+  listen: boolean
+  loadblock: string
+  logtimestamps: boolean
+  maxconnections: string
+  maxreceivebuffer: string
+  maxsendbuffer: string
+  mininput: string
+  onlynet: string
+  paytxfee: string
+  pid: string
   port: string
+  printtoconsole: boolean
+  printtodebugger: boolean
+  proxy: string
+  rescan: boolean
+  rpcallowip: string
+  rpcconnect: string
   rpcpassword: string
   rpcport: string
+  rpcssl: boolean
+  rpcsslcertificatechainfile: string
+  rpcsslciphers: string
+  rpcsslprivatekeyfile: string
   rpcuser: string
+  salvagewallet: boolean
+  seednode: string
+  server: boolean
+  shrinkdebugfile: boolean
+  socks: string
+  staking: boolean
+  synctime: boolean
+  testnet: boolean
+  timeout: string
+  tor: string
+  upgradewallet: boolean
+  upnp: boolean
+  wallet: string
+  walletnotify: string
 }
 
 export interface Settings {
