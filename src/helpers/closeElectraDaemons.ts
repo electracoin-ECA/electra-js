2ab49c0f2766eb5f30f5fd254152a31b9251609b
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-13 23:16:08 +0200

    helpers/closeElectraDaemons: improve & fasten logic

--- src/helpers/closeElectraDaemons.ts
+++ src/helpers/closeElectraDaemons.ts
@@ -1,28 +1,11 @@
 // tslint:disable
 
-import to from 'await-to-js'
+import log from '@inspired-beings/log'
 
 import { DAEMON_CONFIG_DEFAULT, DAEMON_URI } from '../constants'
 import Rpc from '../libs/rpc'
-import isPortAvailable from './isPortAvailable'
+import checkDaemons from './checkDaemons'
 import wait from './wait'
-
-async function exec(command: string): Promise<string> {
-  return new Promise((resolve: (stdout: string) => void, reject) => {
-    require('child_process').exec(
-      command,
-      (err: Error, stdout: string, stderr: string): void => {
-        if (err !== null || stderr.length !== 0) {
-          reject(err || new Error(stderr))
-
-          return
-        }
-
-        resolve(stdout)
-      }
-    )
-  })
-}
 
 function toArrayOfLines(output: string): string[] {
   return output
@@ -32,56 +15,54 @@ function toArrayOfLines(output: string): string[] {
 }
 
 export default async function(): Promise<void> {
-  if (await isPortAvailable(Number(DAEMON_CONFIG_DEFAULT.rpcport))) return
+  if (!(await checkDaemons()).isRunning) return
 
+  // First we try to stop it via a RPC call
+  await stopDaemonViaRpc()
+
+  const res = await checkDaemons()
+  if (!res.isRunning) return
+
+  const outputLines: string[] = toArrayOfLines(res.output as string)
+  const processId: number = process.platform === 'win32'
+    ? Number((outputLines[0].match(/\d+$/) as RegExpMatchArray)[0])
+    : Number((outputLines[0].match(/\s(\d+)/) as RegExpMatchArray)[1])
+
+  // Then we try to kill its process
+  await killDaemonViaProcessId(processId)
+}
+
+async function killDaemonViaProcessId(processId: number): Promise<void> {
+  process.kill(processId)
+
+  // Limit the process killing attempt to 5s
+  let timeLeft: number = 5000
+  while ((await checkDaemons()).isRunning) {
+    await wait(250)
+    timeLeft -= 250
+    if (timeLeft <= 0) break
+  }
+}
+
+async function stopDaemonViaRpc(): Promise<void> {
   const rpc = new Rpc(DAEMON_URI, {
     password: DAEMON_CONFIG_DEFAULT.rpcpassword,
     username: DAEMON_CONFIG_DEFAULT.rpcuser,
   })
 
-  // First, we can try to send a simple "stop" command
   try {
     await rpc.stop()
 
-    // Limit the clean stop attempt to 5s
+    // Limit the RPC stop call attempt to 5s
     let timeLeft: number = 5000
-    while (!await isPortAvailable(Number(DAEMON_CONFIG_DEFAULT.rpcport))) {
+    while ((await checkDaemons()).isRunning) {
       await wait(250)
       timeLeft -= 250
       if (timeLeft <= 0) break
     }
   }
   catch(err) {
-    console.error(err)
+    log.info("The following error is automatically handled. You can safely ignore it.")
+    log.warn(err)
   }
-
-  if (process.platform === 'win32') {
-    // Last resort force kill
-    await to(exec(
-      `FOR /F "tokens=4 delims= " %%P IN ('netstat -a -n -o ^| findstr :${DAEMON_CONFIG_DEFAULT.rpcport}') DO TaskKill.exe /PID %%P`
-    ))
-
-    return
-  }
-
-  const [err, stdout] = await to(exec(`lsof | grep :${DAEMON_CONFIG_DEFAULT.rpcport}`))
-  if (err === null && stdout !== undefined) {
-    const results1 = toArrayOfLines(stdout)
-    // If we find at least one used RPC port, we can try to send a "stop" command
-    if (results1.length !== 0) {
-      const daemonPid = Number(results1[0].split(/\s+/)[1])
-
-      try { await rpc.stop() }
-      catch(err) { /* We can ignore any error here. */ }
-
-      // tslint:disable-next-line:no-require-imports
-      require('tree-kill')(daemonPid, 'SIGKILL')
-    }
-  }
-
-  if (await isPortAvailable(Number(DAEMON_CONFIG_DEFAULT.rpcport))) return
-
-  // Last resort force kill
-  // TODO Is it a possible case after a tree-kill ?
-  await to(exec(`ps -ef | grep -i ^electra* | grep -v grep | awk '{print $2}' | xargs -r kill -9`))
 }
