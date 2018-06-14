27eec6ac42676795f370d3fe99bd8534bdd57850
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-14 02:27:03 +0200

    0.19.1

--- dist/index.js
+++ dist/index.js
@@ -105,7 +105,7 @@ exports.SETTINGS_DEFAULT = {
  * ElectraJs version.
  * DO NOT CHANGE THIS LINE SINCE THE VERSION IS AUTOMATICALLY INJECTED !
  */
-const VERSION = '0.19.0';
+const VERSION = '0.19.1';
 /**
  * Main ElectraJS class.
  */
