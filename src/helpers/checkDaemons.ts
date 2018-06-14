8f9f74e63f228619dfceaf9bd901aa00fa460a01
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-14 07:43:38 +0200

    helpers/checkDaemons: fix TIME_WAIT cases on Windows

--- src/helpers/checkDaemons.ts
+++ src/helpers/checkDaemons.ts
@@ -8,7 +8,8 @@ import { DAEMON_CONFIG_DEFAULT } from '../constants'
  */
 export default async function(): Promise<{ isRunning: boolean, output?: string }> {
   const [err, output] = process.platform === 'win32'
-    ? await to(exec(`netstat -a -n -o | findstr :${DAEMON_CONFIG_DEFAULT.port}`))
+    // https://ss64.com/nt/findstr.html
+    ? await to(exec(`netstat -a -n -o | findstr LISTENING | findstr :${DAEMON_CONFIG_DEFAULT.port}`))
     : await to(exec(`lsof -n -i4TCP:${DAEMON_CONFIG_DEFAULT.port} | grep LISTEN`))
 
   return {
