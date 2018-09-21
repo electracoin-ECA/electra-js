03d824c451a34c087b50a2dbc4a8edb5c630b682
Ruanne Lloyd <ruannelloyd@gmail.com>
2018-09-21 22:22:14 +0200

    0.21.1

--- dist/index.js
+++ dist/index.js
@@ -105,7 +105,7 @@ exports.SETTINGS_DEFAULT = {
  * ElectraJs version.
  * DO NOT CHANGE THIS LINE SINCE THE VERSION IS AUTOMATICALLY INJECTED !
  */
-const VERSION = '0.21.0';
+const VERSION = '0.21.1';
 /**
  * Main ElectraJS class.
  */
